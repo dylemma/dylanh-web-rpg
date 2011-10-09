199eb81569ea8ab8bca6ca74caca75b972ee11ce
pianomanDylan <pianomanDylan@localhost>
2011-10-08 22:15:31 -0400

    Removed BloomMode and deleted the commented-out version of CombatField

--- src/main/scala/com/dylan/combat/CombatField.scala
+++ src/main/scala/com/dylan/combat/CombatField.scala
@@ -2,21 +2,6 @@ package com.dylan.combat
 
 import scala.collection.JavaConversions._
 
-//class CombatField(val graph: Graph[CombatPosition, Adjacency]) {
-//
-//	def positions: Set[CombatPosition] = graph.vertexSet.toSet
-//
-//	def containsPosition(pos: CombatPosition) = graph.containsVertex(pos)
-//
-//	def findPath(start: CombatPosition, end: CombatPosition, limit: Double = Double.MaxValue) = {
-//		val pathFinder = new DijkstraShortestPath(graph, start, end, limit)
-//		pathFinder.getPath match {
-//			case null => None
-//			case path => Some(path)
-//		}
-//	}
-//}
-
 trait CombatField {
 	def positions: Set[CombatPosition]
 	def combattants: List[Combattant]
